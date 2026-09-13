26f6aa238812b1b197ffef3a8d36f6c587765626
Sampleless0 <Sampleless0@gmail.com>
2026-09-13 10:54:28 +0800

    added extension

---
+++ extension/content.js
@@ -0,0 +1,28 @@
+async function loadAssistant() {
+  if (document.getElementById("classroom-assistant-button")) {
+    return;
+  }
+
+  const htmlURL = chrome.runtime.getURL("menu.html");
+  const response = await fetch(htmlURL);
+  const html = await response.text();
+
+  const container = document.createElement("div");
+  container.id = "classroom-assistant-container";
+  container.innerHTML = html;
+
+  document.body.appendChild(container);
+
+  const button = document.getElementById("classroom-assistant-button");
+  const buttonImg = document.getElementById("assistant-button-img");
+  const popup = document.getElementById("classroom-assistant-popup");
+
+  buttonImg.src = chrome.runtime.getURL("mascot/anim/7s_idle.gif");;
+
+  // Open / close popup
+  button.addEventListener("click", () => {
+    popup.classList.toggle("open");
+  });
+}
+
+loadAssistant();
